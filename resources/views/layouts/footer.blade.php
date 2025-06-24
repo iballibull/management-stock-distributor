53077ff91b8502f1a1dad5f15074135ada877c85
iballibull <muchammadiqbalyusuf@gmail.com>
2025-06-24 21:06:07 +0700

    feat: create initial dashboard layout with sidebar and header

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class=" dark:bg-gray-800 border-t text-center py-4 text-sm text-gray-500">
+    <p class="my-10 text-sm text-center text-gray-500">
+        © 2025- {{ date('Y') }} <a href="https://github.com/iballibull" class="hover:underline"
+            target="_blank">iballibull</a>. All rights
+        reserved.
+    </p>
+</footer>
