0c85ec1b680b1445e8403da5aa112e38b2f79a4a
iballibull <muchammadiqbalyusuf@gmail.com>
2025-06-29 16:01:33 +0700

    fead: add initial layout and styles for table categori, education and curriculum view template

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -51,7 +51,7 @@
                             Status
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Action
+                            Aksi
                         </th>
                     </tr>
                 </thead>
