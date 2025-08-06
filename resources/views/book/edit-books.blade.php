b69321ca49c05cf415e6f260fce66ab299279b22
iballibull <muchammadiqbalyusuf@gmail.com>
2025-08-06 14:35:04 +0700

    fix: default value update book price

--- resources/views/book/edit-books.blade.php
+++ resources/views/book/edit-books.blade.php
@@ -125,8 +125,8 @@
                                 <span class="text-red-500">*</span></label>
                             <input type="number" name="price" id="price"
                                 class="{{ $errors->has('price') ? 'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500' : 'bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-600 focus:border-blue-600' }} text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Harga Buku" autocomplete="off" value="{{ old('price', $book->price) }}"
-                                required>
+                                placeholder="Harga Buku" autocomplete="off"
+                                value="{{ old('price', number_format($book->price, '0', ',', '')) }}" required>
                             @error('price')
                                 <p class="mt-2 text-sm text-red-600 dark:text-red-500">
                                     <span class="font-medium">{{ $message }}</span>
